7702fa1ede89fc6406c2a69a399b3817391f3684
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-01-24 19:12:39 +0300

    feat: cli tasks complete

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const res = [];
+
+    for (let i = 0; i < args.length; i += 2) {
+        res.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+    }
+
+    console.log(res.join(', '));
 };
 
 parseArgs();
