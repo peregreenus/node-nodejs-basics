1c9c2395ff72026626f1d42f7193a743ba8a9c7c
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-01-25 18:22:09 +0300

    feat: zip task complete

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archivePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(filePath);
+    const ws = fs.createWriteStream(archivePath);
+    const gzip = createGzip();
+
+    pipeline(
+        rs,
+        gzip,
+        ws,
+        (err) => err
+    );
 };
 
 await compress();
