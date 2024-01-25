7a2bbd065e3e2022c91d9d06ab5144d895be6926
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-01-25 06:29:56 +0300

    feat: hash task complete

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from 'crypto';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const hash = createHash('sha256').setEncoding('hex');
+    const rs = fs.createReadStream(
+        path.join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+    );
+
+    rs.pipe(hash).pipe(stdout);
 };
 
 await calculateHash();
