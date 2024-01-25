25dd58666173b8fd327222189735141a531bd634
Aliaksandr Andreichuk <coldlycalculatedsoul@gmail.com>
2024-01-25 16:02:42 +0300

    feat: streams task complete

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { stdout } from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const rs = fs.createReadStream(
+        path.join(__dirname, 'files', 'fileToRead.txt'),
+        { encoding: 'utf8' }
+    );
+
+    rs.pipe(stdout);
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,31 @@
+import { EOL } from 'os';
+import { Transform, pipeline } from 'stream';
+import { stdin, stdout } from 'process';
+
+
+class Reversed extends Transform {
+    constructor() {
+        super();
+    }
+
+    _transform(chunk, encoding, cb) {
+        chunk = [...chunk.toString()]
+            .reverse()
+            .join('')
+            .trim() + EOL;
+
+        cb(null, chunk);
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reversed = new Reversed();
+    pipeline(
+        stdin,
+        reversed,
+        stdout,
+        (err) => err
+    );
 };
 
 await transform();
